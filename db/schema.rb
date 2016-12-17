7ad094fbb3c642e55ced4db9320748df4b822a37
Aleph Retamal <retamal.ph@gmail.com>
2016-12-18 01:31:51 +1100

    Added AdminLTE to the angular app, closes #18

--- db/schema.rb
+++ db/schema.rb
@@ -54,25 +54,25 @@ ActiveRecord::Schema.define(version: 20161213233827) do
   end
 
   create_table "volunteers", force: :cascade do |t|
-    t.string   "first_name",             limit: 40,                 null: false
+    t.string   "first_name",             limit: 40,                    null: false
     t.string   "last_name",              limit: 40
-    t.string   "email",                  limit: 255,                null: false
+    t.string   "email",                  limit: 255,                   null: false
     t.string   "mobile_number",          limit: 20
     t.string   "landline_number",        limit: 20
-    t.date     "dob",                                               null: false
+    t.date     "dob",                                                  null: false
     t.string   "wwccn",                  limit: 15
     t.boolean  "sub_newsletter",                     default: true
-    t.string   "encrypted_password",                                null: false
+    t.string   "encrypted_password",                                   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,    null: false
+    t.integer  "sign_in_count",                      default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
     t.string   "provider",                           default: "email", null: false
     t.string   "uid",                                default: "",      null: false
     t.json     "tokens"
